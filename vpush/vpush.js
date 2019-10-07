13630df812311526fc134e68317aa3efb64d1c08
Mr.right <1448377877@qq.com>
2019-10-07 20:21:11 +0800

    first commit

---
+++ vpush/vpush.js
@@ -0,0 +1,25 @@
+const user = require('../../common/user.js')
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    vPushHandler: function(e) {
+      user.saveFormId(e.detail.formId)
+    }
+  }
+})
